fae57c6145bc483c7fd16dbca265a1daa931a02d
Ritik kumar <94630724+RITIKGITHUB18@users.noreply.github.com>
2024-03-07 21:00:51 +0530

    Update mail.ts

--- lib/mail.ts
+++ lib/mail.ts
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const domain = process.env.NEXT_PUBLIC_APP_URL;
 
 export const sendVerificationEmail = async (email: string, token: string) => {
-  const confirmLink = `${domain}/auth/new-verification?token=${token}`;
+  const confirmLink = `https://next-auth-ten-bice.vercel.app/auth/new-verification?token=${token}`;
 
   await resend.emails.send({
     from: "mail@quarkk.site",
@@ -16,7 +16,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
 };
 
 export const sendPasswordResetEmail = async (email: string, token: string) => {
-  const resetLink = `${domain}/auth/new-password?token=${token}`;
+  const resetLink = `https://next-auth-ten-bice.vercel.app/auth/new-password?token=${token}`;
 
   await resend.emails.send({
     from: "mail@quarkk.site",
